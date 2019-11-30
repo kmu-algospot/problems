3c64ce2097aa910c6a9d891ec6d1000d0c2fdac6
zaeval <zaeval@among.software>
2019-12-01 01:59:18 +0900

    Update zaeval.cpp
    책을 참조하고 필요없는 소스를 지웠습니다.

--- BOGGLE/zaeval.cpp
+++ BOGGLE/zaeval.cpp
@@ -17,23 +17,24 @@ const int DIRECT_MAX = 8;
 const int DELTA_X[] = {-1, -1, -1, 0, 0, 1, 1, 1};
 const int DELTA_Y[] = {0, 1, -1, 1, -1, 0, 1, -1};
 
-int dfs(int x, int y,int level,string &word,vector<string> &map){
-    if(level >= word.length()){
-        return level;
-    }
+bool hasWord(int x, int y,int level,string &word,vector<string> &map){
+
     if(x >= MAX_SIZE || y >= MAX_SIZE || x < 0 || y < 0){
-        return level;
+        return false;
+    }
+    if(level >= word.length()){
+        return true;
     }
     if(map[y][x] != word[level]){
-        return level;
+        return false;
     }
     
-    int maxLevel = 0;
-    for(int direct = 0; direct < DIRECT_MAX && maxLevel < word.length(); ++direct){
-        maxLevel = max(dfs(x+DELTA_X[direct],y+DELTA_Y[direct],level+1,word,map),maxLevel);
+    for(int direct = 0; direct < DIRECT_MAX; ++direct){
+        if(hasWord(x+DELTA_X[direct],y+DELTA_Y[direct],level+1,word,map))
+            return true;
     }
 
-    return maxLevel;
+    return false;
 }
 int main(){
     int C;
@@ -54,7 +55,7 @@ int main(){
             bool noCheck = true;
             for(int row = 0; row<MAX_SIZE && noCheck;++row){
                 for(int col = 0; col<MAX_SIZE && noCheck; ++col){
-                    if(dfs(col,row,0,word,in) == word.length()){
+                    if(hasWord(col,row,0,word,in)){
                         printf("%s YES\n",word.c_str());
                         noCheck=false;
                     }
