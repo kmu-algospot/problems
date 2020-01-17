69baaf00f703aed94fd8e19b3fd25ae8da64747c
H0zzae <39540561+H0zzae@users.noreply.github.com>
2020-01-17 19:24:16 +0900

    Update H0zzae.cpp

--- QUADTREE/H0zzae.cpp
+++ QUADTREE/H0zzae.cpp
@@ -1,52 +1,22 @@
 #include <iostream>
 using namespace std;
-#define FOR(var, ed) for(int var = 0 ; var<ed; var++)
 
-bool xIsNext(string inputTree){
-    if(inputTree.substr(0,1)=="x"){
-        return true;
+//x xwwwb (xw xwbbb ww) (x (x xwwbb bww) wwb) b
+
+string quadTree(string inputTree,int index){
+    if (inputTree[index]=='w'||inputTree[index]=='b'){
+        return inputTree.substr(index,1);
     }
-    return false;
-}
-string quadTree(string inputTree){
-    if (inputTree=="w"||inputTree=="b"){
-        return inputTree;
-    }
-    if(inputTree.substr(0,1)=="x"){
-         inputTree = inputTree.substr(1);
-    }
-    string firstString;
-    if (xIsNext(inputTree)){
-        firstString = quadTree(inputTree.substr(0,5));
-        inputTree = inputTree.substr(5);
-    }else {
-        firstString = quadTree(inputTree.substr(0,1));
-        inputTree = inputTree.substr(1);
-    }
-    string secondString;
-    if (xIsNext(inputTree)){
-        secondString = quadTree(inputTree.substr(0,5));
-        inputTree = inputTree.substr(5);
-    }else {
-        secondString = quadTree(inputTree.substr(0,1));
-        inputTree = inputTree.substr(1);
-    }
-    string thirdString;
-    if (xIsNext(inputTree)){
-        thirdString = quadTree(inputTree.substr(0,5));
-        inputTree = inputTree.substr(5);
-    }else {
-        thirdString = quadTree(inputTree.substr(0,1));
-        inputTree = inputTree.substr(1);
-    }
-    string fourthString;
-    if (xIsNext(inputTree)){
-        fourthString = quadTree(inputTree.substr(0,5));
-        inputTree = inputTree.substr(5);
-    }else {
-        fourthString = quadTree(inputTree.substr(0,1));
-        inputTree = inputTree.substr(1);
-    }
+    index++;
+
+    string firstString=quadTree(inputTree,index);
+    index+=firstString.length();
+    string secondString=quadTree(inputTree,index);
+    index+=secondString.length();
+    string thirdString=quadTree(inputTree,index);
+    index+=thirdString.length();
+    string fourthString=quadTree(inputTree,index);
+    
     string ret = 'x'+thirdString+fourthString+firstString+secondString;
     return ret;
 }
@@ -57,6 +27,6 @@ int main(){
     while(test--){
         string inputTree;
         cin>>inputTree;
-        cout<<quadTree(inputTree)<<endl;
+        cout<<quadTree(inputTree,0)<<endl;
     }
 }
