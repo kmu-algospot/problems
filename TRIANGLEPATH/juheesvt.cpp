ea6ce2be1c39d1e933d119e090129dc2726a05e8
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2020-02-29 03:25:05 +0900

    Update juheesvt.cpp

--- TRIANGLEPATH/juheesvt.cpp
+++ TRIANGLEPATH/juheesvt.cpp
@@ -1,22 +1,24 @@
 //
 // Created by juheeSVT on 2020-02-29.
-//
+// 메모이제이션 실패
 
 #include <iostream>
 #include <algorithm>
 
 const int MAX = 100;
 int map[MAX][MAX];
+int cache[MAX][MAX];
 
-int triangle(int r, int c, int sum,int triangleSize) {
+int triangle(int r, int c,int triangleSize) {
 
-    int localSum = sum;
+    // 내려가면서 sum을 비교하는 것이 아니기때문에 localSum이 필요없음 !!
+    if ( r == triangleSize - 1 )
+        return map[r][c];
 
-    if (r==triangleSize)
-        return localSum;
+    if ( cache[r][c] != -1 )
+        return cache[r][c];
 
-    localSum += map[r][c];
-    return std::max(triangle(r+1,c,localSum,triangleSize), triangle(r+1,c+1,localSum,triangleSize));
+    return cache[r][c] = std::max(triangle(r+1, c, triangleSize), triangle(r+1, c+1,triangleSize)) + map[r][c];
 
 }
 
@@ -35,8 +37,13 @@ int main(){
             }
         }
 
+        for (int i = 0; i < triangleSize; i++ ) {
+            for (int j = 0; j <= i; j++ ) {
+                cache[i][j] = -1;
+            }
+        }
 
-        printf("%d\n", triangle(0,0,0,triangleSize));
+        printf("%d\n", triangle(0,0,triangleSize));
 
     }
 }
