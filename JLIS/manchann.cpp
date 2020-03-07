356972059d3fb6d350269077fded8629a8a767a3
최재강 <chl8273@naver.com>
2020-03-08 03:34:48 +0900

    Update manchann.cpp

--- JLIS/manchann.cpp
+++ JLIS/manchann.cpp
@@ -2,9 +2,9 @@
 #include <cstring>
 using namespace std;
 
-
+const int MIN = -9999;
 const int MAX = 101;
-int A[100],B[100];
+int A[100],B[100] = {0,};
 int cache[MAX][MAX] = {-1,};
 int JLIS(int a, int b, int sizesA,int sizesB);
 
@@ -12,6 +12,13 @@ int main(){
     int cases;
     cin>> cases;
     while(cases--){
+        for(int i=0; i<MAX;i++){
+            for(int j=0;j<MAX;j++){
+                cache[i][j] = -1;
+            }
+        }
+        memset(A,0,sizeof(A));
+        memset(B,0,sizeof(B));
         int sizesA,sizesB;
         cin>>sizesA;
         cin>>sizesB;
@@ -22,31 +29,27 @@ int main(){
         for(int i =0; i<sizesB;i++){
             cin>> B[i];
         }
-        for(int a=0; a<sizesA; a++){
-            for(int b=0; b<sizesB;b++){
-                maxJLIS = max(maxJLIS,JLIS(a,b,sizesA,sizesB));
-            }
-        }
-        cout<<maxJLIS<<"\n";
+        cout<<JLIS(-1,-1,sizesA,sizesB)<<"\n";
     }
 
 }
 
 int JLIS(int a, int b, int sizesA,int sizesB){
-    int& ret = cache[a][b];
-    if(ret != 1) return ret;
-    ret = 2;
-    int maxNum = max(A[a],B[b]);
+    int& ret = cache[a+1][b+1];
+    if(ret != -1) return ret;
+    ret = 0;
+    int aNum = (a == -1 ? MIN : A[a]);
+    int bNum =  (b == -1 ? MIN : B[b]);
+    int maxNum = max(aNum,bNum);
     for(int i=a+1;i<sizesA;i++){
         if(maxNum < A[i] ){
-            ret = max(maxNum,JLIS(i,b,sizesA,sizesB)+1);
+            ret = max(ret,JLIS(i,b,sizesA,sizesB)+1);
         }
     }
     for(int i=b+1;i<sizesB;i++){
         if(maxNum < B[i] ){
-            ret = max(maxNum,JLIS(a,i,sizesA,sizesB)+1);
+            ret = max(ret,JLIS(a,i,sizesA,sizesB)+1);
         }
     }
-    cout<<ret;
     return ret;
 }
