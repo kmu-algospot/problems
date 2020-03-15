6d88c2ec319bc0daec01c222fda3dce64556abc4
최재강 <chl8273@naver.com>
2020-03-15 12:44:22 +0900

    Update manchann.cpp

--- QUANTIZE/manchann.cpp
+++ QUANTIZE/manchann.cpp
@@ -3,8 +3,8 @@
 #include <cmath>
 using namespace std;
 
-const int ArrayMax = 101;
-const int GroupMax = 11;
+const int ArrayMax = 100;
+const int GroupMax = 10;
 int cache[ArrayMax][GroupMax] ={-1,};
 int n,s;
 int arr[ArrayMax] = {0,};
@@ -16,19 +16,27 @@ int average(int start,int end){
     }
     double average = sum / end-start+1;
     average = floor(average+0.5);
-    return int (average);
+    int ret = 0;
+	for (int i = start; i <= end; i++) {
+		int error = abs(average - arr[i]);
+		ret += error * error;
+	}
+
+	return ret;
 }
 
 int quantization(int start,int group){
+    if(start == n) return 0;
+    if(group == 0) return 99999999;
     int &ret = cache[start][group];
     if(ret != -1){
         return ret;
     }
     
     ret = 99999999;
-    for(int i=start; i<n-s;i++){
-        int quan = average(start,i);
-        ret = min(ret, quantization(i,group-1)+quan);
+    for(int i=1; i<=n-start;i++){
+        int quan = average(start,start+i-1);
+        ret = min(ret, quantization(i+start,group-1)+quan);
     }
     return ret;
 }
@@ -38,14 +46,11 @@ int main(){
     cin>> cases;
     while(cases--){
         cin>>n>>s;
+        memset(cache, -1, sizeof(cache));
         for(int i=0; i< n; i++){
             cin>> arr[i];
         }
         sort(arr,arr+n);
-        int minNum = 99999999;
-        for(int i=0; i< n-s;i++){
-            minNum = min(minNum,quantization(i,s));
-        }
-        cout<<minNum<<"\n";
+        cout<<quantization(0,s)<<"\n";
     }
 }
