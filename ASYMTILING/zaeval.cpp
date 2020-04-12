bcb9282f2dc0b3a1574282cd86095f88ed15f692
zaeval <zaeval@among.software>
2020-04-12 15:11:29 +0900

    Update zaeval.cpp

--- ASYMTILING/zaeval.cpp
+++ ASYMTILING/zaeval.cpp
@@ -3,37 +3,29 @@
 using namespace std;
 
 const int MOD = 1000000007;
-int cache[101];
-int tiling(int width){
-    if(width <= 1)return 1;
-    if(cache[width] != -1) return cache[width];
-    return cache[width] = (tiling(width-2) + tiling(width-1)) % MOD;
-}
+int cache[101]={0,1,2};
+
 int main() {
     
     int C, N;
     
     cin >> C;
-    
+    for(int i=3; i<=100; ++i){
+        cache[i] = (cache[i-1] + cache[i-2])%MOD;
+    }
     while(C--){
         cin >> N;
-        for(int i=0; i<=N;++i){
-            cache[i] = -1;
-        }
+       
         if(N<=2){
             cout << 0 << endl;
         }
         else{
             if(N%2){
-                cout << tiling(N) - tiling(N/2) << endl;
+                cout << cache[N] - cache[N/2] << endl;
             }
             else{
-                int all = tiling(N);
-                int sym = (((long long)tiling(N/2-1)) + tiling(N/2)) % MOD;
-                int asym = all - sym;
-                if(asym < 0)
-                    asym += MOD;
-            
+                int asym = cache[N] - (cache[N/2-1] + cache[N/2])%MOD;
+                if(asym < 0 )asym += MOD;
                 cout << asym << endl;
             }
         }
