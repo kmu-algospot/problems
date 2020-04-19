cf25c47291813e6b8ce9ed6518f2ab70f68b3f0b
zaeval <zaeval@among.software>
2020-04-19 10:59:15 +0900

    Update zaeval.cpp

--- POLYOMIO/zaeval.cpp
+++ POLYOMIO/zaeval.cpp
@@ -4,26 +4,26 @@ using namespace std;
 
 const int MOD = 10000000;
 const int MAX = 100;
-int cache[MAX+1] = { 0 };
+int cache[MAX+1][MAX+1] = { 0 };
+int combi[MAX + 1][MAX + 1] = { 0 };
 int poly(int count, int tot,bool isFirst) {
-	//0 2
 	if (tot == 0 && !isFirst) {
 		return 1;
 	}
-	if (cache[count] != -1) {
-		return cache[count];
+	if (cache[tot][count] != -1) {
+		return cache[tot][count];
 	}
-	
-	
+		
 	int sum = 0;
 	int gop = 1;
-	int nowStart = (isFirst) ? tot : (tot<count)?tot:count;
-	for (int i = nowStart; i >= 1; --i) {
-		if (!isFirst) gop = (count - i + 1);
+
+	for (int i = tot; i >= 1; --i) {
+		if (!isFirst) gop = count+i-1;
 		sum = (sum+  (gop * poly(i, tot - i,false))%MOD)%MOD;
+		cache[tot][count] = sum;
 	}
 	
-	return cache[count] = sum;
+	return cache[tot][count];
 }
 int main() {
 	int C, n;
@@ -33,7 +33,9 @@ int main() {
 	while (C--) {
 		scanf("%d", &n);
 		for (int i = 0; i <= MAX; ++i) {
-			cache[i] = -1;
+			for (int j = 0; j <= MAX; ++j) {
+				cache[i][j] = -1;
+			}
 		}
 		cout << poly(0, n,true) << endl;
 	}
