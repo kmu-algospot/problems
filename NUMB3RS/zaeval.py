07ce33beb2a3c7b9d889f06a2bf0c0dd69cfa079
zaeval <zaeval@among.software>
2020-04-26 13:01:52 +0900

    Update zaeval.py

--- NUMB3RS/zaeval.py
+++ NUMB3RS/zaeval.py
@@ -1,45 +1,40 @@
 C = int(input())
+N, D, P = (0, 0, 0)
+
+
+def search(here, days, cache, adj_map):
+    if days == 0:
+        return 1 if here == P else 0
+
+    if cache[here][days] > -0.5:
+        return cache[here][days]
+
+    cache[here][days] = 0
+
+    for there in adj_map[here]["list"]:
+        cache[here][days] += search(there, days - 1, cache, adj_map) / adj_map[there]["len"]
+
+    return cache[here][days]
+
 
 while C:
     C -= 1
+    global N, D, P
     N, D, P = map(lambda x: int(x), input().split())
 
     mapp = []
     adj_map = []
-    towns = [[-1 for j in range(D+1)] for i in range(N)]
-    queue = [{"town": P, "level": 1}]
+    cache = [[-1 for j in range(D + 1)] for i in range(N)]
 
     for i in range(N):
         mapp.append(list(map(lambda x: int(x), input().split())))
-
         adj_map.append({"len": 0, "list": []})
         for j in range(N):
             if mapp[i][j] == 1:
                 adj_map[i]["list"].append(j)
                 adj_map[i]["len"] += 1
 
-    while len(queue) > 0:
-        top = queue[0]
-        queue = queue[1:]
-        nowTown = top["town"]
-        nowLevel = top["level"]
-
-        for node in adj_map[nowTown]["list"]:
-            size = adj_map[nowTown]["len"]
-            nowP = towns[nowTown][nowLevel-1]
-
-            if nowLevel == 1:
-                nowP = 1
-
-            if towns[node][nowLevel] != -1:
-                towns[node][nowLevel] += nowP * 1 / size
-            else:
-                towns[node][nowLevel] = nowP * 1 / size
-
-            if top["level"] + 1 <= D:
-                queue.append({"town": node, "level": top["level"] + 1})
     T = int(input())
-
     for Q in map(lambda x: int(x), input().split()):
-        print(0 if towns[Q][D] == -1 else towns[Q][D], end=" ")
+        print(search(Q, D, cache, adj_map),end=" ")
     print()
