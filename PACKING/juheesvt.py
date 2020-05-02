88a953e9f9979ed1fca360c6dfcf0b3814900a10
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2020-05-02 23:41:04 +0900

    fail

---
+++ PACKING/juheesvt.py
@@ -0,0 +1,36 @@
+from copy import deepcopy
+
+def main() :
+
+    test_case = int(input())
+
+    for case in range(test_case) :
+        product_num, max_weight = map(int, input().split(" "))
+        product = []                #[물건이름, 부피, 절박도]
+        cache = [[0,[]] for i in range(max_weight+1)]        #[[총 절박도, [물건목록 리스트]]
+
+        for i in range(product_num) :
+            product.append(input().split(" "))
+            product[i][1] = int(product[i][1])
+            product[i][2] = int(product[i][2])
+
+        answer = 0
+        max = 0
+        for w in range(max_weight+1):
+            for p in product:
+                if p[1] <= w :
+                    if (cache[w][0] < cache[w-p[1]][0] + p[2]) and not p in cache[w-p[1]][1] :
+                        cache[w][0] = cache[w-p[1]][0] + p[2]
+                        cache[w][1].clear()
+                        for i in range(len(cache[w-p[1]][1])):
+                            cache[w][1].append(cache[w-p[1]][1][i])
+                        cache[w][1].append(p)
+
+                        if max < cache[w][0]:
+                            max = cache[w][0]
+                            answer = w
+
+        print(max, len(cache[answer][1]))
+        for p in cache[answer][1]:
+            print(p[0])
+main()
